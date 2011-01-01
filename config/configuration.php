e72d734bdf0e63e89dbf27cb2396acb6ce99b48b
Sorrow <chokoteam@gmail.com>
2011-01-01 14:56:18 +0100

    Changement du nom de fichier de connexion MySQL

--- config/configuration.php
+++ config/configuration.php
@@ -5,7 +5,7 @@ define( 'PHP', strrchr( __FILE__, '.' ) );
 define( 'VERSION', '0.0.2' );
 ini_set('date.timezone', 'Europe/Paris');
 
-require_once( 'mysql' . PHP );
+require_once( 'database' . PHP );
 require_once( 'functions' . PHP );
 
 $title = 'Wazuras &bull; Dhuum CMS v' . VERSION;
